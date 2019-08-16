753e709b8dd7db6126f06ab2ce17dfcfceac6694
WhiteLie1 <1136462446@qq.com>
2019-08-16 23:27:18 +0800

    8.16

---
+++ America_python/廖雪峰python/错误调试和测试/mydict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/8/16 17:32
+# @Author : chenxin
+# @Site : 
+# @File : mydict.py
+# @Software: PyCharm
+class Dict(dict):
+    def __int__(self,**kw):
+    def __getattr__(self, key):
+        except KeyError:
+            raise AttributeError
--- America_python/廖雪峰python/错误调试和测试/test.py
+++ America_python/廖雪峰python/错误调试和测试/test.py
@@ -92,13 +92,14 @@ def bar():
 # main()
 
 #logging
-import logging
-logging.basicConfig(level=logging.INFO)
-s = '0'
-n = int(s)
-logging.info('n=%d'% n)
-print(10/n)
+# import logging
+# logging.basicConfig(level=logging.INFO)
+# s = '0'
+# n = int(s)
+# logging.info('n=%d'% n)
+# print(10/n)
 #这就是logging的好处，它允许你指定记录信息的级别，有debug，info，warning，error等几个级别，当我们指定level=INFO时，logging.debug就不起作用了。同理，指定level=WARNING后，debug和info就不起作用了。这样一来，你可以放心地输出不同级别的信息，也不用删除，最后统一控制输出哪个级别的信息。
+
 
 
 
