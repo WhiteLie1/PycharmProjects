2e335a112ee0e0b14ade632bc1ffcb58c7c51492
WhiteLie1 <1136462446@qq.com>
2019-08-24 10:00:27 +0800

    echart的使用

---
+++ AItest/mongotest/pages/bar/bar.js
@@ -0,0 +1,97 @@
+// pages/bar/bar.js
+const app = getApp()
+import * as echarts from '../echarts-for-weixin/ec-canvas/echarts.js'
+let chart = null;
+function initChart(canvas,width,height){
+  chart = echarts.init(canvas,null,{
+    width:width,
+    height:height
+  });
+canvas.setChart(chart);
+let option={
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+},
+yAxis: {
+    type: 'value'
+},
+series: [{
+    data: [820, 932, 901, 934, 1290, 1330, 1320],
+    type: 'line'
+}]
+};
+chart.setOption(option);
+return chart;
+};
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ec:{
+      onInit:initChart
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    setTimeout(function(){
+      console.log(chart)
+    },2000)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
